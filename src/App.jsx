76d1947114d1f3b494ce18fa7044af3aeb12d75a
Swaby404 <swabyoliver@gmail.com>
2025-06-15 14:29:29 -0400

    Unit3 Block24

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+import './index.css'
+
  
- 
- 
-import './App.css'
 
 function App() {
+
+
+const [puppies] = useState(puppyList);
+   console.log("puppyList: ", puppyList);
+   
+const [featPupId, setFeatPupId] = useState(null);
+
+    const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+      console.log(featuredPup)
+  
+    
+
+    
+
+ return (
+    <div className="App">
+      <h1>Puppy List</h1>
+  
+           
+<table>
+    <thead>
+        <tr>
+            <th>Puppy Names</th>
+        </tr>
+    </thead> 
+</table>
+      
+       {puppies.map((puppy) => {
+         return (
+           <button onClick={() => {setFeatPupId(puppy.id)}}
+              key={puppy.id}>{puppy.name}</button>
+         );
+       })}
+         {featPupId && (
+    <div>
+<h2>{featuredPup.name}</h2>
+
+         <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+        </ul>
+    </div>
+      )} 
+       
+
+    </div>
+    
+  );
+}
+ 
+ 
  
 
-  return (
-    <>
-      <div>
-         
-      </div>
-      
-    </>
-  )
-}
 
 export default App
